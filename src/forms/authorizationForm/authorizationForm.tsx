0d891326d5f57e6f5ac2f48c2efdc85838184017
DyeMeat <borisow.dmitry12@gmail.com>
2023-12-10 00:35:53 +0400

    Router and connections added

--- src/forms/authorizationForm/authorizationForm.tsx
+++ src/forms/authorizationForm/authorizationForm.tsx
@@ -1,31 +1,27 @@
-import Header from '../../common/el/header'
-import RumButton from '../../common/el/rumButton'
-import Input from '../../common/el/inputAndTxt'
-import "./authorizationForm.css"
+import Header from "../../common/el/header";
+import RumButton from "../../common/el/rumButton";
+import Input from "../../common/el/inputAndTxt";
+import "./authorizationForm.css";
+import { NavLink } from "react-router-dom";
 
 export default function authorizationForm() {
+  function func() {}
 
-    function func(){}
+  return (
+    <div className="card">
+      <Header username="ASSer" rating={1000} />
+      <Input bigText="Введите логин:" lilText="" />
+      <Input bigText="Введите пароль:" lilText="" />
 
-    return (
-      <div className="card">
-      <Header 
-        username='USer'
-        rating={1000}
-      />
-      <Input
-        bigText='Введите логин'
-        lilText=''
-      />
-      <Input
-        bigText='Введите пароль'
-        lilText=''
-      />
-      <RumButton 
-        text = {"Вход"}
-        func = {func}
-      />
-      
+
+
+      <NavLink to="/main">
+        <RumButton text={"Войти"} func={func} />
+      </NavLink>
+
+      <NavLink to="/registration">
+        <span className="or-register">или зарегистрироваться</span>
+      </NavLink>
     </div>
-    );
+  );
 }
--- src/forms/registrationForm/registrationForm.tsx
+++ src/forms/registrationForm/registrationForm.tsx
@@ -1,31 +1,31 @@
-import Header from '../../common/el/header'
-import RumButton from '../../common/el/rumButton'
-import Input from '../../common/el/inputAndTxt'
-import "../authorizationForm/authorizationForm.css"
+import Header from "../../common/el/header";
+import RumButton from "../../common/el/rumButton";
+import Input from "../../common/el/inputAndTxt";
+import "./registrationForm.css";
+import { NavLink } from "react-router-dom";
+
 
 export default function registrationForm() {
+  function func() {}
 
-    function func(){}
-
-    return (
-      <div className="card">
-      <Header 
-        username='USer'
-        rating={1000}
+  return (
+    <div className="card">
+      <Header username="ASSer" rating={1000} />
+      <Input
+        bigText="Придумайте логин"
+        lilText="от 4 до 16 латинских букв, цифр и подчёркиваний"
       />
       <Input
-        bigText='Придумайте логин'
-        lilText='от 4 до 16 латинских букв, цифр и подчёркиваний'
+        bigText="Введите пароль"
+        lilText="от 4 до 16 латинских букв, цифр и подчёркиваний"
       />
-      <Input
-        bigText='Введите пароль'
-        lilText='от 4 до 16 латинских букв, цифр и подчёркиваний'
-      />
-      <RumButton 
-        text = {"Зарегистрироваться"}
-        func = {func}
-      />
-      
+      <NavLink to="/main">
+        <RumButton text={"Зарегистрироваться"} func={func} />
+      </NavLink>
+
+      <NavLink to="/">
+        <span className="or-authorize">или авторизоваться</span>
+      </NavLink>
     </div>
-    );
+  );
 }
