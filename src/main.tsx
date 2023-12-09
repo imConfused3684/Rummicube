5ba60f1c1af5eaab92e11f769141caf558337c86
imConfused3684 <oleggrirorev@yandex.ru>
2023-12-09 19:54:28 +0400

    удалил всё из app.css и index.css, в index.css 2 глобальные переменные цвета, в main.tsx изменил выбор root

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+//import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.body!).render(
+ReactDOM.createRoot(document.querySelector('.customRoot')!).render(
 
     <App />,
 )
