d0a31017c8c056f3abd22daf37cc2fb433b8ba65
DyeMeat <borisow.dmitry12@gmail.com>
2023-12-20 00:34:25 +0400

    removed loop dependence between board & cell

--- client/src/common/el/models/board.ts
+++ client/src/common/el/models/board.ts
@@ -11,7 +11,8 @@ export class Board {
         for (let i = 0; i < 8; i++) {
             const row: Cell[] = []
             for (let j = 0; j < 23; j++) {
-                row.push(new Cell(this, j, i, null, j == 4 || j == 9))
+                // row.push(new Cell(this, j, i, null, j == 4 || j == 9))
+                row.push(new Cell(j, i, null, j == 4 || j == 9))
             }
             this.cells.push(row);
         }
--- client/src/common/el/models/cell.ts
+++ client/src/common/el/models/cell.ts
@@ -1,30 +1,32 @@
-import {Chip} from "./chip";
-import {Board} from "./board";
- 
+import { Chip } from "./chip";
+// import { Board } from "./board";
+
 export class Cell {
     readonly x: number;
     readonly y: number;
     chip: Chip | null;
-    board: Board;
+    // board: Board;
     availbale: boolean;
     id: number;
     isDivider: boolean;
 
-    constructor(board: Board, x: number, y: number, chip: Chip | null, isDivider: boolean) {
-        this.x = x;
-        this.y = y;
-        this.chip = chip;
-        this.board = board;
-        this.id = Math.random();
-        this.availbale = false;
-        this.isDivider = isDivider;
-    }
+    // constructor(board: Board, x: number, y: number, chip: Chip | null, isDivider: boolean) {
+    constructor(x: number, y: number, chip: Chip | null, isDivider: boolean) {
 
-    moveChip(target: Cell){
-        if(this.chip && this.chip.canMove(target)){
-            this.chip.moveChip(target);
-            target.chip = this.chip;
-            this,this.chip = null;
+            this.x = x;
+            this.y = y;
+            this.chip = chip;
+            // this.board = board;
+            this.id = Math.random();
+            this.availbale = false;
+            this.isDivider = isDivider;
+        }
+
+        moveChip(target: Cell){
+            if (this.chip && this.chip.canMove(target)) {
+                this.chip.moveChip(target);
+                target.chip = this.chip;
+                this, this.chip = null;
+            }
         }
     }
-}
