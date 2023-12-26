65ba93965aed397e45f96d91c2e38460513148d8
imConfused3684 <oleggrirorev@yandex.ru>
2023-12-26 15:01:28 +0400

    prev merge problems fix

--- client/src/forms/firstForm/firstForm.tsx
+++ client/src/forms/firstForm/firstForm.tsx
@@ -24,6 +24,7 @@ export default function FirstForm() {
 
   const showSystemInfo = () => {
     window.open('/system-info', '_blank');
+  }
 
   function goToRoomCustomizationForm() {
     if (uName != null) {
